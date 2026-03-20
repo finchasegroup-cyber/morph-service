9f83bfbb810db4fe86385a00d13d7f21dca98948
finchasegroup-cyber <finchase.group@gmail.com>
2026-03-20 09:53:04 +0300

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import pymorphy2
 from cachetools import TTLCache
@@ -85,4 +86,5 @@ def health():
     return "OK"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
